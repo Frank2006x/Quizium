6cbf99359050b2e7a6f65c22c88e3918b1731f59
Frank2006x <frank2006x@gmail.com>
2025-07-17 19:06:00 +0530

    fix:reverse the sidebar list

--- src/app/api/topics/route.ts
+++ src/app/api/topics/route.ts
@@ -10,8 +10,7 @@ export async function GET(req: NextRequest) {
     req.cookies.get("__Secure-authjs.session-token")?.value;
   const { userId } = await SessionModel.findOne({ sessionToken: token });
 
-  const topic = await quizModal.find({ userId });
-  
+  const topic = (await quizModal.find({ userId })).reverse();
 
   return NextResponse.json(topic);
 }
