fedf23fa9e0b5709ee733bb33df661d76942d372
Frank2006x <frank2006x@gmail.com>
2025-07-26 23:16:06 +0530

    fixed warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "next",
+    "next/typescript"
+  ),
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
